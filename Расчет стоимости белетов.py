188ea6d936b0643f87e7e1d26ce3cd6ac20a6dc8
Posedaev <128963703+Posedaev@users.noreply.github.com>
2023-05-10 02:37:06 +0700

    Update Расчет стоимости белетов.py

--- Расчет стоимости белетов.py
+++ Расчет стоимости белетов.py
@@ -1,12 +1,17 @@
+ticket_sum =0
 tickets = int(input("Введите нужное количество билетов: "))
-age = int(input("Сколько вам лет? "))
-if age <18:
-    print("Проходит бесплатно")
-elif (age >= 18 and age <= 24) and (tickets > 3):
-    print("К оплате: ", int((990 - 990 * 10 / 100) * tickets), "Рублей")
-elif age >= 18 and age <= 24:
-    print("К оплате: ", tickets * 990, "Рублeй")
-elif age > 24 and tickets > 3:
-    print("К оплате: ", int((1390 - 1390 * 10 / 100) * tickets), "Рублeй")
-else:
-    print("К оплате: ", tickets * 1390, "Рублeй")
+for age in range(tickets):
+    age = int(input("Сколько вам лет? "))
+    if age < 18:
+        print("Проходит бесплатно")
+        ticket_sum = ticket_sum+1
+    elif (age >= 18 and age <= 24) and (tickets <=3):
+        print("К оплате: ", 990, "Рублeй")
+        ticket_sum =ticket_sum+1
+    elif tickets >3 and age < 24:
+        print("К оплате: ", int(990 - 990 * 10 / 100), "Рублeй")
+    elif age > 24 and (tickets <=3):
+        print("К оплате: ", 1390, "Рублeй")
+        ticket_sum =ticket_sum+1
+    elif tickets >3:
+        print("К оплате: ", int(1390 - 1390 * 10 / 100), "Рублeй")

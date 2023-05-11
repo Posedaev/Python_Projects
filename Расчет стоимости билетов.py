2bd1de8e87e4d7d55a47dfef57a268000e4237a5
Posedaev <128963703+Posedaev@users.noreply.github.com>
2023-05-11 12:42:18 +0700

    Расчет стоимости билетов.py

--- Расчет стоимости билетов.py
+++ Расчет стоимости билетов.py
@@ -1,3 +1,4 @@
+tickets_sum = 0
 tickets = int(input("Введите нужное количество билетов: "))
 for age in range(tickets):
     age = int(input("Сколько вам лет? "))
@@ -5,9 +6,15 @@ for age in range(tickets):
         print("Проходит бесплатно")
     elif (age >= 18 and age <= 24) and (tickets <=3):
         print("К оплате: ", 990, "Рублeй")
+        tickets_sum = tickets_sum +990
     elif tickets >3 and age < 24:
         print("К оплате: ", int(990 - 990 * 10 / 100), "Рублeй")
+        tickets_sum = tickets_sum +891
     elif age > 24 and (tickets <=3):
         print("К оплате: ", 1390, "Рублeй")
+        tickets_sum = tickets_sum +1390
     elif tickets >3:
         print("К оплате: ", int(1390 - 1390 * 10 / 100), "Рублeй")
+        tickets_sum = tickets_sum +1251
+print("----------------------------->")
+print ("Всего билетов", tickets, "на сумму", tickets_sum)
